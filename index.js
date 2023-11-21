0b3dc5f6d35cf3427f2acf7aa299df531eacf6fb
Agnes RUKUNDO <agnesrukundo1998@gmail.com>
2023-11-21 16:33:03 +0200

    Js practices

---
+++ index.js
@@ -0,0 +1,16 @@
+//variable declaration
+var myName = "Agnes RUKUNDO";
+console.log(myName);
+ 
+//some operators
+var a= 20;
+let b = 15;
+var remainder;
+remainder = a%b;
+console.log(remainder);
+console.log("Hello World This is me!!");
+
+//concantenation
+let firstName = "Agnes";
+let lastName = "RUKUNDO";
+console.log("I am called: "+ firstName+ " " +lastName);
