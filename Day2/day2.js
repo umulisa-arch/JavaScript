603d1818855fc23d0077d436e09b5d9d45fa8650
Agnes RUKUNDO <agnesrukundo1998@gmail.com>
2023-11-22 16:36:00 +0200

    adding string assignment

--- Day2/day2.js
+++ Day2/day2.js
@@ -1,4 +1,36 @@
-//using comppariosn operators
+//using compariosn operators
 let num1 = 12;
 let num2 = 10;
 console.log(num1===num2);
+
+let ag1 = 101;
+let age2 = 100;
+console.log(ag1>=age2);
+
+let name1 = "Aime";
+let name2 = "Aime";
+console.log(name1==name2);
+console.log(name1!=name2);
+
+const numOne = 3;
+const numTwo = 2;
+const numThree = 2;
+console.log(numOne >= numTwo, numTwo >= numThree);
+
+
+//chenking type of values
+let number = 12;
+console.log(number + " is "+ typeof number);
+let number1 = 1.8;
+console.log(typeof number1);
+
+let names= "Agnes RUKUNDO";
+console.log(typeof names);
+
+// string assginment
+let text = "hello";
+console.log(text);
+text = 'hi';
+console.log(text);
+text = `bye`;
+console.log(text);
