ec5a4491a2b36511e16b01c0160ff7f4397c108f
Agnes RUKUNDO <agnesrukundo1998@gmail.com>
2023-11-23 11:20:01 +0200

    day3 added and some

---
+++ Day3/day3.js
@@ -0,0 +1,28 @@
+//conditionals
+let temperature = 70;
+console.log(`The temperature is: ${temperature}`);
+ 
+if (temperature > 70) {
+   console.log("It's warm outside");
+}
+else if(temperature<70){
+    console.log('it is cold outside!');
+}
+else{
+    console.log("it is an average temperature");
+}
+
+//age conditions using if 
+
+let age = 17;
+console.log(`Your age is: ${age}`);
+
+if(age==18){
+    console.log('You become mature now');
+}
+else if(age>18){
+    console.log('You are above maturity age');
+}
+else{
+    console.log('You are still young');
+}
