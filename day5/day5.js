665b26fc3b3498143cad73e183bb6784ddb7cac0
Agnes RUKUNDO <agnesrukundo1998@gmail.com>
2023-11-28 11:29:44 +0200

    day5 added

---
+++ day5/day5.js
@@ -0,0 +1,39 @@
+let name1 = "Rose";
+
+let unitPrice1 = 8;
+
+let quantity1= 70;
+
+value1  = unitPrice1* quantity1;
+
+console.log(name1 + " - unit price:" + unitPrice1 + " ,"+ "quantity: " + quantity1 + ", "  + "value: " + value1 );
+
+
+
+
+let name2 = "Lily";
+
+let unitPrice2 = 10;
+
+let quantity2= 50;
+
+value2  = unitPrice2 * quantity2;
+
+console.log(name2 + " - unit price:" + unitPrice2 + ","+ "quantity: " + quantity2 + ","  + "value: " + value2 );
+
+
+
+
+let name3 = "Tulip";
+
+let unitPrice3 = 2;
+
+let quantity3= 120;
+
+value3 = unitPrice3 * quantity3;
+
+console.log(name3+ " - unit price:" + unitPrice3 + ","+ "quantity: " + quantity3 + ","  + "value: " + value3 );
+
+Total = value1 + value2+value3;
+
+console.log("Total: " + Total);
