2a81255234a3e5547ba3fd3dcac05a816f4c9417
Agnes RUKUNDO <agnesrukundo1998@gmail.com>
2023-11-29 13:23:26 +0200

    som changes made

--- day5/day5.js
+++ day5/day5.js
@@ -6,7 +6,7 @@ let quantity1= 70;
 
 value1 = unitPrice1* quantity1;
 
-console.log(name1 + " - unit price:" + unitPrice1 + " ,"+ "quantity: " + quantity1 + ", " + "value: " + value1 );
+console.log(name1 + " - unit price:" + unitPrice1 + ", "+ "quantity: " + quantity1 + ", " + "value: " + value1 );
 
 
 
@@ -19,7 +19,7 @@ let quantity2= 50;
 
 value2 = unitPrice2 * quantity2;
 
-console.log(name2 + " - unit price:" + unitPrice2 + ","+ "quantity: " + quantity2 + "," + "value: " + value2 );
+console.log(name2 + " - unit price:" + unitPrice2 + ", "+ "quantity: " + quantity2 + ", " + "value: " + value2 );
 
 
 
@@ -32,7 +32,7 @@ let quantity3= 120;
 
 value3 = unitPrice3 * quantity3;
 
-console.log(name3+ " - unit price:" + unitPrice3 + ","+ "quantity: " + quantity3 + ","  + "value: " + value3 );
+console.log(name3+ " - unit price:" + unitPrice3 + ", "+ "quantity: " + quantity3 + ", "  + "value: " + value3 );
 
 Total = value1 + value2+value3;
 
--- day6/day6.js
+++ day6/day6.js
@@ -106,3 +106,15 @@ let  message1  =  'The  vessel  \'Mars\'  called  at  the  port.';
  someResource  =  null;
  console.log(someResource);  //  ->  null
  console.log(typeof  someResource);  //  ->  object
+
+ //default values of data types
+ const str = String();
+ const num = Number();
+ const bool = Boolean();
+
+console.log(str); // ->
+console.log(num); // -> 0
+console.log(bool); // -> false
+
+ const big1 = BigInt(42);
+ console.log(big1); // -> 42n
