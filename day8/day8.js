2622c6926c22daa47cbbdf688363058bd15e1aca
Agnes RUKUNDO <agnesrukundo1998@gmail.com>
2023-12-05 09:39:30 +0200

    day8 added

---
+++ day8/day8.js
@@ -0,0 +1,15 @@
+let number = prompt("Enter a number", 0);
+        if (number < 10) {
+     alert("The number is less than 10");
+         } else if (number < 30) {
+     alert("The number is less than30");
+        } else if (number < 60) {
+     alert("The number is less than60");
+        } else if (number < 90) {
+     alert("The number is less than90");
+        } else if (number < 100) {
+     alert("<100");
+    } else if (number == 100) {
+        alert("The number is equals to 100")
+    } else {
+     alert("The number is greater than 100")}
