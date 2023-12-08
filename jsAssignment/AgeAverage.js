be245e39d8360dfcdb2dd7e17d7a17151e60a495
Agnes RUKUNDO <agnesrukundo1998@gmail.com>
2023-12-08 13:12:53 +0200

    average of students age

---
+++ jsAssignment/AgeAverage.js
@@ -0,0 +1,14 @@
+//Average age of the entered students
+let student = [];
+let sumAge = 0;
+let avgAge = 0;
+for (let i = 0; i <= 26; i++){
+    let age = prompt("enter your age");
+    let userAge = Number(age);
+    student.push(userAge);
+    sumAge += student[i];
+    avgAge = sumAge/student.length;
+}
+
+alert("the average :" + avgAge);
+
