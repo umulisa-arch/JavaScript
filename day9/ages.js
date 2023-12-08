65eeb6d05b4f60824ce9a714f59b0b910de8ae1f
Agnes RUKUNDO <agnesrukundo1998@gmail.com>
2023-12-08 12:10:00 +0200

    power

--- day9/ages.js
+++ day9/ages.js
@@ -9,3 +9,5 @@ for (let i = 0; i < numberOfStudents; i++) {
 let averageAge = totalAges / numberOfStudents;
 
 console.log("Average age of students: " + averageAge);
+
+//second way
--- jsAssignment/power.js
+++ jsAssignment/power.js
@@ -3,7 +3,8 @@ let exponent = parseFloat(prompt("Enter the exponent:"));
 
 if (!isNaN(base) && !isNaN(exponent)) {
     let result = Math.pow(base, exponent);
-    console.log(`${base} power ${exponent} is: ${result}`);
+    
+    console.log(base +" power "+exponent+ " is: "+result);
 } else {
     console.log("Please enter e base and exponent.");
 }
