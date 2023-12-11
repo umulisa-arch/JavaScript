86828fd5aec8814063d758d1612bdaba387a7047
Agnes RUKUNDO <agnesrukundo1998@gmail.com>
2023-12-11 12:39:25 +0200

    update

--- day10/temp2.js
+++ day10/temp2.js
@@ -1,18 +1,20 @@
-let temp;
+let temp = [];
 let numT;
 let sum;
 let meanTemp = 0;
+function getTemp(){
 numT = Number(prompt("How many datatemp do you want ? ", numT));
 for(i=0;i<numT;i++){
-    temp[i]= Number(prompt("Enter temp "+i+":"));
+    temp[i]= Number(prompt(`Enter temp: ${i}`));
+}
 }
 function getmeanTemp(){
      sum = 0;
     for(let i = 0;i<temp.length;i++){
-        prompt("enter temp: " + i + temp[i]);
-        sum+=temp;
+        sum+=temp[i];
     }
     meanTemp = sum/temp.length;
 }
-getmeanTemp([12, 13, 40, 52]);
+getTemp();
+getmeanTemp();
 console.log(`Mean temp: ${meanTemp}`);// ` means back tick
