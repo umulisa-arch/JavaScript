6b2dcdea45c01714d61d5a4597f690a7f23cdb45
Agnes RUKUNDO <agnesrukundo1998@gmail.com>
2023-12-11 11:23:41 +0200

    add temp new way

--- day10/temp.js
+++ day10/temp.js
@@ -1,13 +1,12 @@
 let temp;
 let sum;
 let meanTemp = 0;
-function getmeanTemp(){
+function getmeanTemp(temp){
     sum = 0;
     for(let i = 0;i<temp.length;i++){
         sum+=temp[i];
     }
     meanTemp = sum/temp.length;
 }
-temp = [12, 13, 40, 52];
-getmeanTemp();
+getmeanTemp([12, 13, 40, 52]);
 console.log(`Mean temp: ${meanTemp}`);// ` means back tick
