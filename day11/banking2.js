09f039564bea8b4a122ce26721b32fde1517c60e
Agnes RUKUNDO <agnesrukundo1998@gmail.com>
2023-12-12 17:10:19 +0200

    update

--- day11/banking2.js
+++ day11/banking2.js
@@ -1,9 +1,26 @@
 let balance = 0;
+
+function balanceCheck(){
+    alert("Your balance is: "+balance);
+}
+balanceCheck();
 function deposit(){
     let amount =  Number(prompt("Enter amount to deposit"));
     if(amount > 0){
         balance += amount;
-alert("Your balance is: "+balance)
+alert("Deposit amount: "+amount+ " Your balance is: "+balance)
     }
 }
 deposit();
+
+function withdraw(){
+    let withdraw =  Number(prompt(" Enter amount to withdraw: "));
+    if(withdraw>balance){
+        alert("Insufficient balance!!");
+    }
+    else{
+        balance-=withdraw;
+        alert("Withdraw amount: "+withdraw+" Your new balance is: " +balance);
+    }
+}
+withdraw();
