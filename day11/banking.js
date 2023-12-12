af3cbbf956e075df6640e3726b2354c4e44ba70c
Agnes RUKUNDO <agnesrukundo1998@gmail.com>
2023-12-12 15:04:08 +0200

    banking system

--- day11/banking.js
+++ day11/banking.js
@@ -4,3 +4,11 @@ let withdraw;
 deposit = Number(prompt("Enter your deposit"));
 balance+=deposit;
 console.log(" Your current balance: ", balance);
+withdraw =  Number(prompt("Enter amount you want to withdraw: "));
+if(withdraw>balance){
+    alert("Insufficient balance!!");
+}
+else{
+    balance-=withdraw;
+    console.log("Withdraw successfuly done, your new balance is : ", balance);
+}
