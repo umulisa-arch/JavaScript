2adb58d65a0a4a8cb66a837d58e1e2b225d1f5e2
Agnes RUKUNDO <agnesrukundo1998@gmail.com>
2023-12-13 10:39:23 +0200

    new day added

--- day12/Math.js
+++ day12/Math.js
@@ -9,18 +9,24 @@ console.log("Absoulte value of  " , nu2, "is ", Math.abs(nu2));
 //power
 console.log("10 power 2 is: ",Math.pow(100,2));
 //round
-console.log(Math.round(4.4));
-console.log(Math.round(4.6));
+console.log("Round of 4.4 is: ",Math.round(4.4));
+console.log("Round of 4.6 is: ",Math.round(4.6));
 //ceil
-console.log(Math.ceil(20.1));
-console.log(Math.floor(20.9));
+console.log("Uproundof 20.00001 is: ",Math.ceil(20.00001));
+
 //floor
+console.log(Math.floor(20.9));
 //exponent
+console.log(Math.exp(2));//it takes constant value e power the given number in brackets
 //logarithm cosine tangent
 // pi
+console.log(Math.PI);
 //random 
-console.log(Math.random(1000));
+console.log(Math.random());// no need to put value in bracket
 //maximum
+console.log(Math.max(20, 30, 122, 250));
 //minimum
+//trunc
+console.log(Math.trunc(200.9));
 
 
