69c69ab4e6f3abfd1b1d8c6dfe36cc99c5652fee
Agnes RUKUNDO <agnesrukundo1998@gmail.com>
2023-12-13 10:01:09 +0200

    new day added

--- day12/Math.js
+++ day12/Math.js
@@ -7,15 +7,19 @@ console.log("Your value is: ", Math.sqrt(num));
 let nu2 = -20;
 console.log("Absoulte value of  " , nu2, "is ", Math.abs(nu2));
 //power
-console.log(Math.pow(100,2));
+console.log("10 power 2 is: ",Math.pow(100,2));
 //round
-console.log(Math.round(4.5));
+console.log(Math.round(4.4));
+console.log(Math.round(4.6));
 //ceil
+console.log(Math.ceil(20.1));
+console.log(Math.floor(20.9));
 //floor
 //exponent
 //logarithm cosine tangent
 // pi
 //random 
+console.log(Math.random(1000));
 //maximum
 //minimum
 
