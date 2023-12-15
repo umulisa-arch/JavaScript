fce6e976e8a05a332a3519d81fbcee080f5a8622
Agnes RUKUNDO <agnesrukundo1998@gmail.com>
2023-12-15 17:40:03 +0200

    update

--- BriceAssign/password.js
+++ BriceAssign/password.js
@@ -1,14 +1,14 @@
+// You can use this generated password for the signup process
 function generatePassword(length) {
     const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
     let password = "";
     for (let i = 0; i < length; i++) {
-      const randomIndex = Math.floor(Math.random() * charset.length);
+      const randomIndex = Math.floor((Math.random() * charset.length));
       password += charset[randomIndex];
     }
     return password;
   }
   
-  // Usage example
-  const generatedPassword = generatePassword(8); // Change 12 to the desired password length
+  const generatedPassword = generatePassword(8); // You can change 8 to the desired password length
   console.log("Generated Password:", generatedPassword);
-  // You can use this generated password for the signup process
+  
