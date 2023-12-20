3ea9cccf26a9f8a5ddb707186d492d9a0406d352
Agnes RUKUNDO <agnesrukundo1998@gmail.com>
2023-12-20 10:39:48 +0200

    some changes made

--- day14/functions.js
+++ day14/functions.js
@@ -7,10 +7,18 @@ function multiply(a,b){
 function subtract(a,b){
     return a-b;
 }
-function operating(operator,num1,num2){
+function divide(a,b){
+    return a/b;
+}
+function modulus(a,b){
+    return a%b;
+}
+function workOut(operator,num1,num2){
     return operator(num1,num2);
    
 }
-operating(add(12,20));
-operating(subtract(30,100));
-operating(multiply(20,8));
+console.log("Additon: ",workOut(add, 12, 20));
+console.log("Subtraction: ",workOut(subtract, 30, 100));
+console.log("Multiplication: ",workOut(multiply, 20, 8));
+console.log("Division: ",workOut(divide, 20, 8));
+console.log("Remainder: ",workOut(modulus, 20, 8));
