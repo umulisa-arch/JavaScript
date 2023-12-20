ccbd7cf5ec9e6818bc131a83bfcd85a67516a4c2
Agnes RUKUNDO <agnesrukundo1998@gmail.com>
2023-12-20 12:50:36 +0200

    some changes made

--- fetch/fetch.js
+++ fetch/fetch.js
@@ -1,11 +1,15 @@
+ const newUser = {
+    name: "Agnes",
+    job: "Developer"
+ };
  fetch(`https://reqres.in/api/users`)
-.then(res =>{
-    console.log(res);
-})
-// .then(res =>res.json())
-
-// .then(data => {
-//     console.log(data);
-//     console.log(data.data[4].first_name, data.data[4].email, data.data[4].last_name);
+// .then(res =>{
+//     console.log(res);
 // })
+.then(res =>res.json())
+
+.then(data => {
+    console.log(data);
+    console.log(data.data[4].first_name, data.data[4].email, data.data[4].last_name);
+})
 
