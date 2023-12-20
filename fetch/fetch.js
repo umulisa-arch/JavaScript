9b195cedf2a54f0d449e2f60b2207d77a2c6edad
Agnes RUKUNDO <agnesrukundo1998@gmail.com>
2023-12-20 12:25:15 +0200

    some changes made

--- fetch/fetch.js
+++ fetch/fetch.js
@@ -1,2 +1,9 @@
-let data = fetch(`https://reqres.in/api/users`);
-console.log(data);
+ fetch(`https://reqres.in/api/users`)
+// .then(res =>{
+//     console.log(res);
+// })
+.then(res =>res.json())
+.then(data => {
+    console.log(data.data[0].first_name);
+})
+
