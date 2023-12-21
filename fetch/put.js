d157bd50a9b8c699031f2958886fafe5337a8716
Agnes RUKUNDO <agnesrukundo1998@gmail.com>
2023-12-21 12:32:12 +0200

    update

--- fetch/put.js
+++ fetch/put.js
@@ -1,8 +1,8 @@
 
    fetch('https://jsonplaceholder.typicode.com/users')
    .then((response) => response.json())
-   .then((data) => 
-   console.log(data[0].username, " ",data[1].username, " ",data[2].username, " ", data[3].username, " ", data[4].username, " ", data[5].username, " ", data[6].username, " ",data[7].username, " ", data[8].username, " ",data[9].username, ));
-
-
+   .then((data) =>{
+    for(i=0;i<data.length;i++)
+   console.log(data[i].username)});
+    
 
