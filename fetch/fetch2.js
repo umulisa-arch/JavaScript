16dc5f48fe9b9c20bdd39a64f08876084fcf982a
Agnes RUKUNDO <agnesrukundo1998@gmail.com>
2023-12-21 10:23:15 +0200

    update

---
+++ fetch/fetch2.js
@@ -0,0 +1,32 @@
+const btnEl = document.querySelector("..btn");
+
+const newUser = {
+   name: 'RUKUNDO',
+   job: 'IT'
+};
+const clickHandler = async
+try {
+    const res = await fetch('https://reqres.in/api/users',{
+        method: 'POST',
+        headers:{
+    }
+
+   
+'Content-Type': 'application/json'
+   },body: JSON.stringify(newUser)
+})
+.then(res =>{
+   if(!res.ok){
+       console.log("Problem occured");
+       return;
+   }
+   return res.json()
+})
+
+.then(data => {
+   console.log("success");
+   // console.log(data.data[4].first_name, data.data[4].email, data.data[4].last_name);
+}).catch(error=>{
+   console.log(error);
+})
+
