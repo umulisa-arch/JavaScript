574907f4aa943853dc5a8a84fa0c895ee5a5ef69
Agnes RUKUNDO <agnesrukundo1998@gmail.com>
2023-12-21 13:05:57 +0200

    update

--- fetch/posting.js
+++ fetch/posting.js
@@ -1,18 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Fetch data from the API
-    fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(data => {
-            // for(let i =0;i<data.length;i++)
-            // Access the user's name from the fetched data
-            const userName = data[1].username;
 
-            // Display the user's name in the browser
-            const userInfoDiv = document.getElementById('user-info');
-           
-            userInfoDiv.textContent = `Welcome, ${userName}!`;
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => {
+            if(!response.ok){
+                console.log("problem");
+                return;
+            }
+            return response.json();
+
         })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
-});
+        .then (data =>{
+            for(let i = 0; i<data.length;i++){
+                console.log(data[i].name)
+            }
+            
+        })
+        
+        
+            
